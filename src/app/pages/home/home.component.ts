b9c19d65b987306f4296e3108eecc39d254871b6
Anthony-cecconi <anthony.cecconi.91@gmail.com>
2025-03-03 17:48:18 +0100

    footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../sections/nav/nav.component';
+import { FooterComponent } from "../../sections/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NavComponent
-  ],
+    NavComponent,
+    FooterComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
